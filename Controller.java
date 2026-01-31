8ae6b7535113b3dd1635c193715d68a4adeb4fb9
DarkFox90 <carlospozuelos3@gmail.com>
2026-01-31 10:30:15 -0600

    agregado metodo para leer archivos .txt

--- Controller.java
+++ Controller.java
@@ -1,3 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Controller {
     
+    public void ReadFile() {
+        String filePath = "";   
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    char c = line.charAt(i);
+                    if (c == ' ') {
+                        continue;
+                    }
+                    
+                    System.out.println(c);
+                } 
+            } 
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no existe");
+        }
+        catch(IOException e) {
+            System.out.println("algo salio mal");
+        }
+    }
 }
