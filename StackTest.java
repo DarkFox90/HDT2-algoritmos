c3ab9b6d9edeb43a279b4d522f6a82e5c4cebf87
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-01-31 15:30:20 -0600

    Segunda prueba a Peek

--- StackTest.java
+++ StackTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 public class StackTest {
 
     @Test
-    public void testPushAndPop(){
+    public void testPushandPopandPeek(){
         Stack<Integer> stack = new ArrayStack<>();
         stack.push(10);
         stack.push(20);
+        assertEquals(Integer.valueOf(20), stack.peek());
         assertEquals(Integer.valueOf(20), stack.pop());
+        assertEquals(Integer.valueOf(10), stack.peek());
         assertEquals(Integer.valueOf(10), stack.pop());  
     };
 }
